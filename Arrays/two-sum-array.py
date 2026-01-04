03a75050f6e3bf307801ab8ce67e442e69e7de90
Shekshavali <sheksha357@gmail.com>
2026-01-04 20:18:09 +0530

    sum of the array

--- Arrays/two-sum-array.py
+++ Arrays/two-sum-array.py
@@ -1,19 +1,11 @@
-"""
-LeetCode 1: Two Sum
-https://leetcode.com/problems/two-sum/
-
-Approach:
-- Use hashmap to store value and index
-- Check if target - current exists
-
-Time Complexity: O(n)
-Space Complexity: O(n)
-"""
-
-def twoSum(nums, target):
-    hashmap = {}
-    for i, num in enumerate(nums):
-        diff = target - num
-        if diff in hashmap:
-            return [hashmap[diff], i]
-        hashmap[num] = i
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
